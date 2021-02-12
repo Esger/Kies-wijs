e9d99c5f36fbfc555bb8e5a434e1e7ae8ed2bbe0
Esger Jellema <esgerj@gmail.com>
2021-02-12 16:44:09 +0100

    Meer stellingen, layout

--- src/components/pizza-list.js
+++ src/components/pizza-list.js
@@ -13,140 +13,146 @@ export class PizzaListCustomElement {
 				"gewicht": "1.00",
 				"nummer": 1,
 				"sortingWeight": 0,
-				"ingredienten": []
+				"ingredienten": ['boerenbedrijven',]
 			},
 			{
 				"naam": "PVV (Partij voor de Vrijheid)",
 				"gewicht": "1.00",
 				"nummer": 2,
 				"sortingWeight": 0,
-				"ingredienten": []
+				"ingredienten": ['boerenbedrijven', 'sociale huurwoningen',]
 			}, {
 				"naam": "CDA",
 				"gewicht": "1.00",
 				"nummer": 3,
 				"sortingWeight": 0,
-				"ingredienten": ['religieus']
+				"ingredienten": ['boerenbedrijven', 'middenschool', 'religieus']
 			}, {
 				"naam": "D66",
 				"gewicht": "1.00",
 				"nummer": 4,
 				"sortingWeight": 0,
-				"ingredienten": []
+				"ingredienten": ['middenschool', 'vluchtelingen opnemen']
 			}, {
 				"naam": "GROENLINKS",
 				"gewicht": "1.00",
 				"nummer": 5,
 				"sortingWeight": 0,
-				"ingredienten": []
+				"ingredienten": ['middenschool', 'sociale huurwoningen', 'vluchtelingen opnemen']
+			}, {
+				"naam": "SP (Socialistische Partij) ",
+				"gewicht": "1.00",
+				"nummer": 6,
+				"sortingWeight": 0,
+				"ingredienten": ['middenschool', 'sociale huurwoningen', 'vluchtelingen opnemen']
 			}, {
 				"naam": "Partij van de Arbeid (P.v.d.A.)",
 				"gewicht": "1.00",
 				"nummer": 7,
 				"sortingWeight": 0,
-				"ingredienten": []
+				"ingredienten": ['middenschool', 'sociale huurwoningen', 'vluchtelingen opnemen']
 			}, {
 				"naam": "ChristenUnie",
 				"gewicht": "1.00",
 				"nummer": 8,
 				"sortingWeight": 0,
-				"ingredienten": ['religieus']
+				"ingredienten": ['~boerenbedrijven', 'sociale huurwoningen', 'religieus', 'vluchtelingen opnemen']
 			}, {
 				"naam": "Partij voor de Dieren",
 				"gewicht": "1.00",
 				"nummer": 9,
 				"sortingWeight": 0,
-				"ingredienten": ['mondkapjesplicht']
+				"ingredienten": ['middenschool', 'mondkapjesplicht', 'sociale huurwoningen', 'vluchtelingen opnemen']
 			}, {
 				"naam": "50PLUS",
 				"gewicht": "1.00",
 				"nummer": 10,
 				"sortingWeight": 0,
-				"ingredienten": []
+				"ingredienten": ['middenschool', 'sociale huurwoningen',]
 			}, {
 				"naam": "Staatkundig Gereformeerde Partij (SGP)",
 				"gewicht": "1.00",
 				"nummer": 11,
 				"sortingWeight": 0,
-				"ingredienten": ['mondkapjesplicht', 'religieus']
+				"ingredienten": ['boerenbedrijven', 'mondkapjesplicht', 'religieus']
 			}, {
 				"naam": "DENK",
 				"gewicht": "1.00",
 				"nummer": 12,
 				"sortingWeight": 0,
-				"ingredienten": ['mondkapjesplicht']
+				"ingredienten": ['middenschool', 'mondkapjesplicht', 'sociale huurwoningen', 'vluchtelingen opnemen']
 			}, {
 				"naam": "Forum voor Democratie",
 				"gewicht": "1.00",
 				"nummer": 13,
 				"sortingWeight": 0,
-				"ingredienten": ['mondkapjesplicht']
+				"ingredienten": ['boerenbedrijven', 'mondkapjesplicht']
 			}, {
 				"naam": "BIJ1",
 				"gewicht": "1.00",
 				"nummer": 14,
 				"sortingWeight": 0,
-				"ingredienten": []
+				"ingredienten": ['middenschool', 'sociale huurwoningen', 'vluchtelingen opnemen']
 			}, {
 				"naam": "JA21",
 				"gewicht": "1.00",
 				"nummer": 15,
 				"sortingWeight": 0,
-				"ingredienten": []
+				"ingredienten": ['boerenbedrijven', '~mondkapjesplicht']
 			}, {
 				"naam": "CODE ORANJE",
 				"gewicht": "1.00",
 				"nummer": 16,
 				"sortingWeight": 0,
-				"ingredienten": ['mondkapjesplicht']
+				"ingredienten": ['boerenbedrijven', 'sociale huurwoningen', 'mondkapjesplicht']
 			}, {
 				"naam": "Volt",
 				"gewicht": "1.00",
 				"nummer": 17,
 				"sortingWeight": 0,
-				"ingredienten": []
+				"ingredienten": ['middenschool', 'vluchtelingen opnemen']
 			}, {
 				"naam": "NIDA",
 				"gewicht": "1.00",
 				"nummer": 18,
 				"sortingWeight": 0,
-				"ingredienten": ['mondkapjesplicht']
+				"ingredienten": ['middenschool', 'sociale huurwoningen', 'mondkapjesplicht', 'vluchtelingen opnemen']
 			}, {
 				"naam": "Piratenpartij",
 				"gewicht": "1.00",
 				"nummer": 19,
 				"sortingWeight": 0,
-				"ingredienten": ['mondkapjesplicht', 'democratische vernieuwing']
+				"ingredienten": ['middenschool', 'democratische vernieuwing', 'sociale huurwoningen', 'mondkapjesplicht', 'vluchtelingen opnemen']
 			}, {
 				"naam": "LP (Libertaire Partij)",
 				"gewicht": "1.00",
 				"nummer": 20,
 				"sortingWeight": 0,
-				"ingredienten": ['mondkapjesplicht']
+				"ingredienten": ['boerenbedrijven', 'mondkapjesplicht']
 			}, {
 				"naam": "JONG",
 				"gewicht": "1.00",
 				"nummer": 21,
 				"sortingWeight": 0,
-				"ingredienten": []
+				"ingredienten": ['sociale huurwoningen',]
 			}, {
 				"naam": "Splinter",
 				"gewicht": "1.00",
 				"nummer": 22,
 				"sortingWeight": 0,
-				"ingredienten": ['mondkapjesplicht']
+				"ingredienten": ['middenschool', 'sociale huurwoningen', 'mondkapjesplicht']
 			}, {
 				"naam": "BoerBurgerBeweging",
 				"gewicht": "1.00",
 				"nummer": 23,
 				"sortingWeight": 0,
-				"ingredienten": ['mondkapjesplicht']
+				"ingredienten": ['boerenbedrijven', 'middenschool', 'mondkapjesplicht']
 			}, {
 				"naam": "NLBeter",
 				"gewicht": "1.00",
 				"nummer": 24,
 				"sortingWeight": 0,
-				"ingredienten": []
+				"ingredienten": ['sociale huurwoningen', 'vluchtelingen opnemen']
 			}, {
 				"naam": "Lijst Henk Krol",
 				"gewicht": "1.00",
@@ -158,13 +164,13 @@ export class PizzaListCustomElement {
 				"gewicht": "1.00",
 				"nummer": 26,
 				"sortingWeight": 0,
-				"ingredienten": ['mondkapjesplicht']
+				"ingredienten": ['boerenbedrijven', 'sociale huurwoningen', '~middenschool', 'mondkapjesplicht']
 			}, {
 				"naam": "JEZUS LEEFT",
 				"gewicht": "1.00",
 				"nummer": 27,
 				"sortingWeight": 0,
-				"ingredienten": ['mondkapjesplicht', 'religieus']
+				"ingredienten": ['boerenbedrijven', 'middenschool', 'mondkapjesplicht', 'religieus']
 			}, {
 				"naam": "Trots op Nederland (TROTS)",
 				"gewicht": "1.00",
@@ -246,11 +252,17 @@ export class PizzaListCustomElement {
 
 	updatePreferences(ingredient, d) {
 		this.showList = !this.showList;
-		for (let party of this.parties) {
-			if (party.ingredienten.includes(ingredient.name.toLowerCase())) {
-				party.sortingWeight += d;
+		this.parties.forEach(party => {
+			if (party.ingredienten.includes('~' + ingredient.name.toLowerCase())) {
+				// Negeren
+			} else {
+				if (party.ingredienten.includes(ingredient.name.toLowerCase())) {
+					party.sortingWeight += d;
+				} else {
+					party.sortingWeight -= d;
+				}
 			}
-		}
+		});
 		setTimeout(() => {
 			this.showList = !this.showList;
 		}, 0);
